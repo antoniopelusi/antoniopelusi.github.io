13650d6e74e7b36128c148518eefa5bfe8b23933
Antonio Pelusi <antoniopelusi2000@gmail.com>
2023-12-05 14:57:39 +0100

    update

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,10 +1,6 @@
 import { basic, initSidebar, initTopbar } from './modules/layouts';
 import { initLocaleDatetime, loadImg } from './modules/plugins';
 
-window.addEventListener("load", (event) => {
-    new cursoreffects.ghostCursor();
-  });
-
 basic();
 initSidebar();
 initTopbar();
