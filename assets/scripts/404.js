c5a059e178b78cc66159126af9bc1d840e4f2e53
Antonio Pelusi <antoniopelusi2000@gmail.com>
2024-12-11 23:28:02 +0100

    Update

--- assets/scripts/404.js
+++ assets/scripts/404.js
@@ -1,23 +1,25 @@
-function changeBackground() {
-    let bsodElement = document.querySelector('bsod'); // Aggiunto il punto per selezionare una classe, se 'bsod' è una classe
-    let originalOpacity = bsodElement.style.opacity || 1; // Se l'opacità non è stata impostata, assumiamo che sia 1
+document.addEventListener("DOMContentLoaded", () => {
+	function changeBackground() {
+		let bsodElement = document.querySelector('bsod'); // Aggiunto il punto per selezionare una classe, se 'bsod' è una classe
+		let originalOpacity = bsodElement.style.opacity || 1; // Se l'opacità non è stata impostata, assumiamo che sia 1
     
-    // Inizializza l'opacità a 1, senza modifiche immediate
-    bsodElement.style.opacity = 1;
+		// Inizializza l'opacità a 1, senza modifiche immediate
+		bsodElement.style.opacity = 1;
 
-    // Aggiungiamo un delay prima di cambiare l'opacità
-    setTimeout(() => {
-        bsodElement.style.opacity = 0.7;
+		// Aggiungiamo un delay prima di cambiare l'opacità
+		setTimeout(() => {
+			bsodElement.style.opacity = 0.7;
         
-        // Ripristina l'opacità originale dopo 200ms
-        setTimeout(() => {
-            bsodElement.style.opacity = originalOpacity;
-        }, 200);
-    }, 1000); // Cambia l'opacità dopo 1 secondo
+			// Ripristina l'opacità originale dopo 200ms
+			setTimeout(() => {
+				bsodElement.style.opacity = originalOpacity;
+			}, 200);
+		}, 1000); // Cambia l'opacità dopo 1 secondo
 
-    let randomTime = Math.floor(Math.random() * 5000) + 1000;
-    setTimeout(changeBackground, randomTime); // Chiamata ricorsiva con intervallo casuale
-}
+		let randomTime = Math.floor(Math.random() * 5000) + 1000;
+		setTimeout(changeBackground, randomTime); // Chiamata ricorsiva con intervallo casuale
+	}
 
-// Avvio della funzione
-changeBackground();
+	// Avvio della funzione
+	changeBackground();
+});
--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -1,107 +1,109 @@
-async function fetchGitHubRepos() {
-	const response = await fetch("/assets/data/repos.json");
+document.addEventListener("DOMContentLoaded", () => {
+	async function fetchGitHubRepos() {
+		const response = await fetch("/assets/data/repos.json");
 
-	const repos = await response.json();
+		const repos = await response.json();
 
-	repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+		repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-	const repoList = document.getElementById("repo-list");
-	repoList.innerHTML = "";
+		const repoList = document.getElementById("repo-list");
+		repoList.innerHTML = "";
 
-	repos.forEach((repo, index) => {
-		const listItem = document.createElement("li");
-		const container = document.createElement("div");
+		repos.forEach((repo, index) => {
+			const listItem = document.createElement("li");
+			const container = document.createElement("div");
 
-		container.style.display = "flex";
-		container.style.justifyContent = "space-between";
-		container.style.alignItems = "center";
+			container.style.display = "flex";
+			container.style.justifyContent = "space-between";
+			container.style.alignItems = "center";
 
-		const titleContainer = document.createElement("div");
-		titleContainer.style.display = "flex";
-		titleContainer.style.alignItems = "center";
-		titleContainer.style.justifyContent = "left";
-		titleContainer.style.wordBreak = "break-all";
+			const titleContainer = document.createElement("div");
+			titleContainer.style.display = "flex";
+			titleContainer.style.alignItems = "center";
+			titleContainer.style.justifyContent = "left";
+			titleContainer.style.wordBreak = "break-all";
 
-		const image = document.createElement("img");
+			const image = document.createElement("img");
 
-		if (index === 0) {
-			image.src = "/assets/icons/listicon/listStart.png";
-		} else if (index === repos.length - 1) {
-			image.src = "/assets/icons/listicon/listEnd.png";
-		} else {
-			image.src = "/assets/icons/listicon/listItem.png";
-		}
-
-		image.style.height = "50px";
-		image.style.minWidth = "22px";
-		image.style.maxWidth = "22px";
-
-		const link = document.createElement("a");
-		link.href = repo.html_url;
-		link.textContent = repo.name;
-		link.target = "_blank";
-
-		const languagesContainer = document.createElement("div");
-		languagesContainer.style.display = "flex";
-		languagesContainer.style.gap = "5px";
-		languagesContainer.style.marginLeft = "auto";
-
-		repo.languages.forEach((language) => {
-			const color = getLanguageColor(language);
-			if (color !== null) {
-				const languageDot = document.createElement("div");
-				languageDot.style.width = "12px";
-				languageDot.style.height = "12px";
-				languageDot.style.borderRadius = "50%";
-				languageDot.style.backgroundColor = color;
-				languageDot.setAttribute("language", language);
-				languageDot.className = "language-dot";
-				languagesContainer.appendChild(languageDot);
+			if (index === 0) {
+				image.src = "/assets/icons/listicon/listStart.png";
+			} else if (index === repos.length - 1) {
+				image.src = "/assets/icons/listicon/listEnd.png";
+			} else {
+				image.src = "/assets/icons/listicon/listItem.png";
 			}
+
+			image.style.height = "50px";
+			image.style.minWidth = "22px";
+			image.style.maxWidth = "22px";
+
+			const link = document.createElement("a");
+			link.href = repo.html_url;
+			link.textContent = repo.name;
+			link.target = "_blank";
+
+			const languagesContainer = document.createElement("div");
+			languagesContainer.style.display = "flex";
+			languagesContainer.style.gap = "5px";
+			languagesContainer.style.marginLeft = "auto";
+
+			repo.languages.forEach((language) => {
+				const color = getLanguageColor(language);
+				if (color !== null) {
+					const languageDot = document.createElement("div");
+					languageDot.style.width = "12px";
+					languageDot.style.height = "12px";
+					languageDot.style.borderRadius = "50%";
+					languageDot.style.backgroundColor = color;
+					languageDot.setAttribute("language", language);
+					languageDot.className = "language-dot";
+					languagesContainer.appendChild(languageDot);
+				}
+			});
+
+			if (index % 2 === 0) {
+				listItem.style.background = "#212124";
+			} else {
+				listItem.style.backgroundColor = "#1b1b1e";
+			}
+
+			titleContainer.appendChild(image);
+			titleContainer.appendChild(link);
+
+			container.appendChild(titleContainer);
+			container.appendChild(languagesContainer);
+			listItem.appendChild(container);
+			repoList.appendChild(listItem);
 		});
+	}
 
-		if (index % 2 === 0) {
-			listItem.style.background = "#212124";
-		} else {
-			listItem.style.backgroundColor = "#1b1b1e";
-		}
+	function getLanguageColor(language) {
+		const colors = {
+			C: "#555555",
+			"C++": "#f34b7f",
+			Rust: "#dea584",
+			"C#": "#178701",
+			Cuda: "#3b4e3a",
+			Java: "#af7219",
+			Kotlin: "#a97bff",
+			Python: "#3573a6",
+			"Jupyter Notebook": "#da5b0c",
+			Perl: "#0398c3",
+			LISP: "#3fb68c",
+			COBOL: "#9e6a04",
+			HTML: "#e44c27",
+			CSS: "#543e7c",
+			SCSS: "#c6538c",
+			JavaScript: "#f1e15a",
+			TypeScript: "#3178c6",
+			Ruby: "#6f1515",
+			Makefile: "#42781a",
+			CMake: "#da3434",
+			Shell: "#89e051",
+			Dockerfile: "#394d54",
+		};
+		return colors[language] || null;
+	}
 
-		titleContainer.appendChild(image);
-		titleContainer.appendChild(link);
-
-		container.appendChild(titleContainer);
-		container.appendChild(languagesContainer);
-		listItem.appendChild(container);
-		repoList.appendChild(listItem);
-	});
-}
-
-function getLanguageColor(language) {
-	const colors = {
-		C: "#555555",
-		"C++": "#f34b7f",
-		Rust: "#dea584",
-		"C#": "#178701",
-		Cuda: "#3b4e3a",
-		Java: "#af7219",
-		Kotlin: "#a97bff",
-		Python: "#3573a6",
-		"Jupyter Notebook": "#da5b0c",
-		Perl: "#0398c3",
-		LISP: "#3fb68c",
-		COBOL: "#9e6a04",
-		HTML: "#e44c27",
-		CSS: "#543e7c",
-		SCSS: "#c6538c",
-		JavaScript: "#f1e15a",
-		TypeScript: "#3178c6",
-		Ruby: "#6f1515",
-		Makefile: "#42781a",
-		CMake: "#da3434",
-		Shell: "#89e051",
-		Dockerfile: "#394d54",
-	};
-	return colors[language] || null;
-}
-
-window.onload = fetchGitHubRepos;
+	window.onload = fetchGitHubRepos;
+});
